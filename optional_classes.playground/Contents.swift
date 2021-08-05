50bb5865245a023f85326940257649b36f9a798a
Мария Корогодова <maria@Mary.local>
2021-08-05 22:41:51 +0300

    optional and classes

--- optional_classes.playground/Contents.swift
+++ optional_classes.playground/Contents.swift
@@ -20,28 +20,96 @@ import UIKit
 //end optional
 
 //classes
+//class Human{
+//    var name: String
+//    var age: Int? = 38
+//    var hairs: Bool
+//    init() {
+//        name = "Ivan"
+//        hairs = true
+//    }
+//    init(name: String, age: Int?, hair: Bool) {
+//        self.name = name
+//        self.age = age
+//        self.hairs = true
+//    }
+//    func description() -> String{
+//        if let humanAge = age{
+//        return "Hello! My name is \(name) and I'm \(age) years old"
+//        } else {
+//            return "Hello! My name is \(name)"
+//        }
+//    }
+
+//}
+//let humanOne = Human()
+//humanOne.name = "Masha"
+//humanOne.name
+//humanOne.description()
+//let humanTwo = Human()
+//humanTwo.name = "Manya"
+//humanTwo.name
+//humanOne.name
+//var array = [Human]()
+//array.append(humanOne)
+//array.append(humanTwo)
+//let himanThree = Human()
+//himanThree.name
+//наследование
+//class Human{
+//    var name: String
+//    init(name: String) {
+//        self.name = name
+//    }
+//    func TellAboutMe()->String{
+//        return "Hello my name is \(name)"
+//    }
+//}
+//class Child: Human{
+//    var toy = "Horse"
+//    override func TellAboutMe() -> String {
+//        let originalText = super.TellAboutMe()
+//        return originalText + " and my toy is \(toy)"
+//    }
+//    init(toy: String, name: String) {
+//        self.toy = toy
+//        super.init(name: name)
+//    }
+//    override init(name: String) {
+//        self.toy = "Barbie"
+//        super.init(name: name)
+//    }
+//}
+//let child1 = Child(toy: "Pony", name: "Mary")
+//print(child1.TellAboutMe())
+//
+//let child2 = Child(name: "Max")
+//child2.toy
+//child2.name
+//можно предотвратить наследование с помощью ключевого слова final
+//например можно поставить это слово перед методом и этот метод нельзя будет переопределить. так же можно запретить наследоваться от класса
+
+//вычисляемые свойства
 class Human{
-    var name = "Ivan"
-    var age: Int? = 38
-    var hairs = true
-    
-    func description() -> String{
-        if let humanAge = age{
-        return "Hello! My name is \(name) and I'm \(age) years old"
-        } else {
-            return "Hello! My name is \(name)"
+    var name: String
+    var secondName: String
+    var fullName: String{
+        get{
+        return name + " " + secondName
+        }
+        set(anotherNewValue){
+            let array = anotherNewValue.components(separatedBy: " ")
+            name = array[0]
+            secondName = array[1]
         }
     }
-
+    init(name: String, secondName: String) {
+        self.name = name
+        self.secondName = secondName
+    }
 }
-let humanOne = Human()
-humanOne.name = "Masha"
-humanOne.name
-humanOne.description()
-let humanTwo = Human()
-humanTwo.name = "Manya"
-humanTwo.name
-humanOne.name
-var array = [Human]()
-array.append(humanOne)
-array.append(humanTwo)
+var human1 = Human(name: "Ivan", secondName: "Ivanov")
+human1.fullName
+human1.fullName = "Zinaida Petrova"
+human1.name
+human1.secondName
